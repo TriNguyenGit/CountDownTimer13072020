b755566b5bd31e66505c2f719d18aa967f8ffa47
TriNguyenGit <nguyenvantri51480153@gmail.com>
2020-07-29 19:11:46 +0700

    Initial commit

---
+++ app/src/main/java/com/example/countdowntimer13072020/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.countdowntimer13072020;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //ctr + pace : gợi ý code
+        //ctr + p : xem tham số truyền vào
+        CountDownTimer countDownTimer = new CountDownTimer(5000,1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                Log.d("BBB",millisUntilFinished +"");
+            }
+
+            @Override
+            public void onFinish() {
+                Log.d("BBB","onFinish");
+            }
+        };
+    }
+}
